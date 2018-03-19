99faba82b2d194993caf34ea66430d0f3c3b5086
Daniel Chen <daniel.chen@utexas.edu>
2018-03-19 14:19:14 -0500

    Changed README

--- app/src/main/java/golden_retriever/qru/RegisterActivity.java
+++ app/src/main/java/golden_retriever/qru/RegisterActivity.java
@@ -1,8 +1,13 @@
 package golden_retriever.qru;
 
-/**
- * Created by daniel on 3/18/18.
- */
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
 
-public class RegisterActivity {
+public class RegisterActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register2);
+    }
 }
