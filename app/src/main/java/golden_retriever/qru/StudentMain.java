4366c7296b70dda3489441463d9869a24fc7617f
Matthew Johnson <mjohnson082396@utexas.edu>
2018-03-26 15:50:30 -0500

    adding studentMain and recruiterMain activities

--- app/src/main/java/golden_retriever/qru/StudentMain.java
+++ app/src/main/java/golden_retriever/qru/StudentMain.java
@@ -1,29 +1,48 @@
 package golden_retriever.qru;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
 
 public class StudentMain extends AppCompatActivity {
-
+    private Button updateProfileButton;
+    private Button newScanButton;
+    private TextView welcomeMessage;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_student_main);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
+        updateProfileButton = (Button) findViewById(R.id.update_recruiter_profile_btn);
+        updateProfileButton.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+
+                // go to student update profile activity
             }
         });
+
+        newScanButton = (Button) findViewById(R.id.scan_button);
+        newScanButton.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view) {
+
+                Intent myIntent = new Intent(StudentMain.this, StaticQR.class);
+                myIntent.putExtra("profiletype", "Student");
+                StudentMain.this.startActivity(myIntent);
+            }
+        });
+
+
     }
 
 }
