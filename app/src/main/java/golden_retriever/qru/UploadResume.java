ac19a364754b9735623eeff39cd8e1e7ba59862f
Daniel Chen <daniel.chen@utexas.edu>
2018-04-02 10:20:18 -0500

    cleanup

--- app/src/main/java/golden_retriever/qru/UploadResume.java
+++ app/src/main/java/golden_retriever/qru/UploadResume.java
@@ -11,7 +11,7 @@ public class UploadResume extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_upload_resume);
-        hello =
-        hello.setText("hello!");
+        //hello =
+        //hello.setText("hello!");
     }
 }
