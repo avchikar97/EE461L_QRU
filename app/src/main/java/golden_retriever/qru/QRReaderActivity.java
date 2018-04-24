e4aae8faec78d22f65f692ff50c619f887f89f5b
Matthew Johnson <mjohnson082396@utexas.edu>
2018-04-24 18:43:24 -0500

    added db calls to QRGen and QRRead activities

--- app/src/main/java/golden_retriever/qru/QRReaderActivity.java
+++ app/src/main/java/golden_retriever/qru/QRReaderActivity.java
@@ -6,6 +6,7 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
 import android.app.Activity;
 import android.graphics.Bitmap;
@@ -26,11 +27,17 @@ import com.google.zxing.integration.android.IntentResult;
 import android.content.Intent;
 import android.widget.Toast;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.concurrent.ExecutionException;
+
 import static android.support.v4.content.ContextCompat.startActivity;
 import static java.net.Proxy.Type.HTTP;
 
-public class QRReaderActivity extends AppCompatActivity {
+public class QRReaderActivity extends AppCompatActivity implements AsyncResponse {
     private Button scan_btn;
+    JSONObject hold = null;
 
 
     @Override
@@ -60,13 +67,52 @@ public class QRReaderActivity extends AppCompatActivity {
                 Toast.makeText(
                         this, "You cancelled the scanning", Toast.LENGTH_LONG).show();
             } else {
-                Toast.makeText(this, result.getContents(), Toast.LENGTH_SHORT).show();
-                    // do database stuff
+                String ID = result.getContents();
+                String firstName = "";
+                String lastName = ""
+
+
+                JSONObject json = getProfile(ID);
+                try{
+                    firstName = json.getString("firstName");
+                    lastName = json.getString("lastName");
+                } catch(Exception e){
+
+                }
+
+                String message = "You just met " + firstName + " " + lastName +  "!";
+                Toast.makeText(this, message, Toast.LENGTH_SHORT).show();
+
+
             }
         } else{
             super.onActivityResult(requestCode, resultCode, data);
         }
     }
+    public JSONObject getProfile(String ID){
+        RestAsync rest = new RestAsync(this);
 
+        JSONObject profile = new JSONObject();
 
+        rest.setType("GET");
+        rest.execute(profile);
+
+        try{
+            hold = rest.get();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } catch (ExecutionException e ) {
+            e.printStackTrace();
+        }
+        if(hold.has("_id")){
+            return hold;
+        } else {
+            return null;
+        }
+    }
+
+    @Override
+    public void processFinish(JSONObject output) {
+        hold = output;
+    }
 }
