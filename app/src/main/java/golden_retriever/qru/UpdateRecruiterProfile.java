6f45c092fbca4bfac3eaa2e081a6644e664b37e1
Akaash Chikarmane <akaash.chikarmane@utexas.edu>
2018-04-29 14:34:33 -0500

    Now display current information as hint

--- app/src/main/java/golden_retriever/qru/UpdateRecruiterProfile.java
+++ app/src/main/java/golden_retriever/qru/UpdateRecruiterProfile.java
@@ -15,9 +15,19 @@ import android.widget.Spinner;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.util.concurrent.ExecutionException;
+
 public class UpdateRecruiterProfile extends AppCompatActivity implements AsyncResponse {
     private static final String[] mongoFields = {"firstName", "lastName", "company", "email", "about", "position"};
+    private static final String[] fieldDefaults =
+            {"First Name",
+            "Last Name",
+            "Company Name",
+            "Email",
+            "About your company",
+            "Positions"};
     private static final int NUM_FIELDS = 6;
+    private static EditText[] fields = new EditText[NUM_FIELDS];
 
     private String[] fieldResults = new String[6];
     private EditText profile_firstname;
@@ -39,16 +49,54 @@ public class UpdateRecruiterProfile extends AppCompatActivity implements AsyncRe
         setSupportActionBar(toolbar);
         ID = getIntent().getStringExtra("ID");
         final RestAsync rest = new RestAsync(this);
+        RestAsync rest2 = new RestAsync(this);
+        rest2.setType("GET");
+        JSONObject idCheck = new JSONObject();
+        JSONObject result = new JSONObject();
+        try{
+            idCheck.put("_id", ID);
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+        rest2.execute(idCheck);
+        try{
+            result = rest2.get();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } catch (ExecutionException e) {
+            e.printStackTrace();
+        }
         rest.setId(ID);
         rest.setType("UPDATE");
 
         profile_firstname = (EditText) findViewById(R.id.recruiter_update_profile_firstname);
+        fields[0] = profile_firstname;
         profile_lastname = (EditText) findViewById(R.id.recruiter_update_profile_lastname);
+        fields[1] = profile_lastname;
         profile_company = (EditText) findViewById(R.id.recruiter_update_profile_company);
+        fields[2] = profile_company;
         email = (EditText) findViewById(R.id.recruiter_update_email);
+        fields[3] = email;
         profile_company_about = (EditText) findViewById(R.id.recruiter_update_profile_company_about);
+        fields[4] = profile_company_about;
         profile_positions = (EditText) findViewById(R.id.recruiter_update_profile_positions);
+        fields[5] = profile_positions;
         submit_profile_btn = (Button) findViewById(R.id.submit_recruiter_profile_btn);
+
+        for(int i = 0; i < NUM_FIELDS; i++){
+            String label = mongoFields[i];
+            if(result.has(label)){
+                String current_result = "";
+                try {
+                    current_result = result.getString(label);
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+                fields[i].setHint(current_result);
+            }
+            else
+                fields[i].setHint(fieldDefaults[i]);
+        }
 
         submit_profile_btn.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -79,6 +127,7 @@ public class UpdateRecruiterProfile extends AppCompatActivity implements AsyncRe
 
                 Intent myIntent = new Intent(UpdateRecruiterProfile.this, RecruiterMain.class);
                 myIntent.putExtra("profiletype", "Recruiter");
+                myIntent.putExtra("ID", ID);
                 UpdateRecruiterProfile.this.startActivity(myIntent);
             }
         });
